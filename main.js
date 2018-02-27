8a493bd6b50a350f5dc6feebdc7fe794d9da69af
Ryan Palmer <ryan.palm93@gmail.com>
2018-02-27 14:30:02 -0800

    added random functionality to questions

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function random(a,b=1) {
+    // if only 1 argument is provided, we need to swap the values of a and b
+    if (b === 1) {
+        [a,b] = [b,a];
+}
+return Math.floor((b-a+1) * Math.random()) + a;
+}
+
+function shuffle(array) {
+    for (let i = array.length; i; i--) {
+        let j = random(i)-1;
+        [array[i - 1], array[j]] = [array[j], array[i - 1]];
+}
+}
+
 const quiz = [
     { name: "Superman",realName: "Clark Kent" },
     { name: "Wonderwoman",realName: "Dianna Prince" },
@@ -68,6 +83,7 @@ countdown() {
 ask(name){
     console.log('ask() invoked');
     if(this.questions.length > 0) {
+    shuffle(this.questions);
     this.question = this.questions.pop();
     const question = `What is ${this.question.name}'s real name?`;
     view.render(view.question,question);
