25c29a0c193888ae12c1c9f2d6658099595bad7c
Ryan Palmer <ryan.palm93@gmail.com>
2018-03-07 14:20:27 -0800

    edited quiz ninja to have multiple button selections.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 'use strict';
 
+// Quiz Ninja game created while following along/ reading "Javascript: Novice to Ninja" book. //
+
 const quiz = [
     { name: "Superman",realName: "Clark Kent" },
     { name: "Wonderwoman",realName: "Dianna Prince" },
     { name: "Batman",realName: "Bruce Wayne" },
+    { name: "The Hulk",realName: "Bruce Banner" },
+    { name: "Spider-man",realName: "Peter Parker" },
+    { name: "Cyclops",realName: "Scott Summers" },
   ];
 
 // Utility functions
@@ -43,10 +48,6 @@ const view = {
     hide(element){
         element.style.display = 'none';
 },
-resetForm(){
-    this.response.answer.value = '';
-    this.response.answer.focus();
-},
 setup(){
     this.show(this.question);
     this.show(this.response);
@@ -55,13 +56,15 @@ setup(){
     this.render(this.score,game.score);
     this.render(this.result,'');
     this.render(this.info,'');
-    this.resetForm();
 },
 teardown(){
     this.hide(this.question);
     this.hide(this.response);
     this.show(this.start);
-    }
+,}
+buttons(array){
+    return array.map(value => `<button>${value}</button>`).join('');
+}
 };
 
 const game = {
@@ -83,19 +86,23 @@ countdown() {
 },
 ask(name){
     console.log('ask() invoked');
-    if(this.questions.length > 0) {
-        shuffle(this.questions);
-        this.question = this.questions.pop();
-        const question = `What is ${this.question.name}'s real name?`;
-        view.render(view.question,question);
-} else {
-    this.gameOver();
+    if(this.questions.length > 2) {
+    shuffle(this.questions);
+    this.question = this.questions.pop();
+const options = [this.questions[0].realName,this.questions[1].realName, this.question.realName];
+    shuffle(options);
+const question = `What is ${this.question.name}'s real name?`;
+    view.render(view.question,question);
+    view.render(view.response,view.buttons(options));
+    }
+    else {
+        this.gameOver();
     }
 },
+
 check(event){
     console.log('check(event) invoked');
-    event.preventDefault();
-    const response = view.response.answer.value;
+    const response = event.target.textContent;
     const answer = this.question.realName;
         if(response === answer){
     view.render(view.result,'Correct!',{'class':'correct'});
@@ -104,7 +111,6 @@ check(event){
 } else {
     view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
 }
-    view.resetForm();
     this.ask();
 },
 gameOver(){
@@ -116,6 +122,5 @@ gameOver(){
 }
 
 view.start.addEventListener('click', () => game.start(quiz), false);
-view.response.addEventListener('submit', (event) => 
+view.response.addEventListener('click', (event) => 
 game.check(event), false);
-view.hide(view.response);
